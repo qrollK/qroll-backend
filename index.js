883a58c46181b636bd833433f2215f729a783228
qrollK <itskrish2654@gmail.com>
2025-08-03 18:51:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,10 +14,8 @@ app.use('/api/session', require('./routes/session'));
 app.use('/api/attendance', require('./routes/attendance'));
 
 // DB Connect
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected'))
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB Connected'))
   .catch((err) => console.log(err));
 
 // Start Server
