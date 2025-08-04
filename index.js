c1de0e6fdd619fa72b0d132a0ae5e9696362531b
qrollK <itskrish2654@gmail.com>
2025-08-04 18:29:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/session', require('./routes/session'));
 app.use('/api/attendance', require('./routes/attendance'));
+app.use("/auth", authRoutes);
 
 
 // DB Connect
