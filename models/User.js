bfed8940e7f4446075f0817bbd972dad8ad0e819
qrollK <itskrish2654@gmail.com>
2025-08-04 20:23:12 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  googleId: { type: String, required: true },
+  googleId: String,
   email: { type: String, required: true, unique: true },
   name: String,
   picture: String,
