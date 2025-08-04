2fd94f1a31100dd51d66cc17c14b142895f31da3
qrollK <itskrish2654@gmail.com>
2025-08-04 17:24:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const authRoutes = require("./routes/authRoutes");
+const authRoutes = require("./routes/auth"); 
 
 dotenv.config();
 const app = express();
