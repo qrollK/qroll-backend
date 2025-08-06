0090a881aaba7fb7d4831f74013934657cfd1feb
qrollK <itskrish2654@gmail.com>
2025-08-06 14:57:49 +0000

    sync

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const studentRoutes = require('./routes/student');
-const sessionRoutes = require('./routes/sessions');
+const sessionRoutes = require('./routes/session');
 
 dotenv.config();
 const app = express();
